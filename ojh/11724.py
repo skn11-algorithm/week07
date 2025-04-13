f6375fcad07c729523578d1a9f6d41e887a34159
Ohjunghh <cmt5ttt@gmail.com>
2025-04-13 16:02:36 +0900

    [ojh] boj: modify 11724.py - modify for loop

--- ojh/11724.py
+++ ojh/11724.py
@@ -2,18 +2,18 @@ import sys
 from collections import deque
 
 def bfs(n):
-    queue=deque()
-    queue.append(n)
+    queue=deque([n])
     visited[n]=True
     while queue:
         x=queue.pop()
-        for i in range(len(graph[x])):
-            if not visited[graph[x][i]]:
-                queue.append(graph[x][i])
-                visited[graph[x][i]]=True
+        for num in graph[x]:
+            if not visited[num]:
+                queue.append(num)
+                visited[num]=True
         
 input=sys.stdin.readline
 N,M = map(int,input().rstrip().split()) #정점, 간선 개수
+
 graph=[[] for _ in range(N+1)]
 for _ in range(M):
     a,b=map(int,input().rstrip().split())
@@ -21,15 +21,12 @@ for _ in range(M):
     graph[b].append(a)
 
 visited=[False]*(N+1)
-
 count=0
+
 for i in range(1,N+1):
     if not visited[i]:
         bfs(i)
         count+=1
 
 print(count)
-
-
-
 
