6c8ccc6f7b91c6ef0eca28d7775c71eb9d79900d
Ohjunghh <cmt5ttt@gmail.com>
2025-04-13 15:54:42 +0900

    [ojh] boj: modify 2667.py - remove visited list

--- ojh/2667.py
+++ ojh/2667.py
@@ -3,17 +3,16 @@ from collections import deque
 
 def bfs(a,b,count):
     queue=deque([(a,b)])
-    visited[a][b]=True
+    graph[a][b]=0
 
     while queue:
         x,y=queue.pop()
         for i in range(4):
             nx,ny=x+dx[i],y+dy[i]
             if 0<=nx<n and 0<=ny<n and graph[nx][ny]==1:
-                if not visited[nx][ny]:
-                    queue.append((nx,ny))
-                    count+=1
-                    visited[nx][ny]=True
+                queue.append((nx,ny))
+                count+=1
+                graph[nx][ny]=0
 
     return count
 
@@ -21,14 +20,13 @@ input=sys.stdin.readline
 n=int(input())
 
 graph=[list(map(int,input().strip())) for _ in range(n)]
-visited=[[False]*n for _ in range(n)]
 dx=[0,0,-1,1]
 dy=[1,-1,0,0]
 
 cnt=[]
 for i in range(n):
     for j in range(n):
-        if graph[i][j]==1 and not visited[i][j]:
+        if graph[i][j]==1:
             cnt.append(bfs(i,j,1))
 
 print(len(cnt))
